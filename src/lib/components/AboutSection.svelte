7fa488e3c326e6eef865d58dd0eadeb13bb3b577
Ethkuil <ethkuil@gmail.com>
2025-08-24 19:52:28 +0800

    feat!: update social media links
    Specific to me.

--- src/lib/components/AboutSection.svelte
+++ src/lib/components/AboutSection.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
-	import Email from '$lib/components/Email.svelte';
-	import Github from '$lib/components/Github.svelte';
-	import Twitter from '$lib/components/Twitter.svelte';
-	import { BIO, EMAIL, GITHUB_URL, TWITTER } from '$lib/constants';
+	import { BIO, EMAIL, GITHUB_URL } from '$lib/constants';
 
-	// todo specify type
-	let components: unknown[] = [];
-
-	GITHUB_URL && components.push(Github);
-	TWITTER && components.push(Twitter);
-	EMAIL && components.push(Email);
+	let medias = {
+		'Email': `mailto:${EMAIL}`,
+		'Github': GITHUB_URL,
+		'Bilibili': 'https://space.bilibili.com/472336190',
+		'Bangumi': 'https://bangumi.tv/user/ethkuil',
+		'Last.fm': 'https://www.last.fm/user/Ethkuil',
+	};
 </script>
 
 <section class="mt-4 flex flex-col gap-4">
 	<p>{BIO}</p>
-	{#if components.length}
-		<p class="flex flex-row items-center gap-1">
-			Find me on {#each components as component, index (component)}
-				<svelte:component this={component} />
-				{index < components.length - 2 ? ', ' : ''}
-				{index === components.length - 2 ? ' and ' : ''}
-			{/each}
-			.
-		</p>
-	{/if}
+
+	Find me on:
+	<ul class="flex list-disc flex-col gap-1 pl-5">
+		{#each Object.entries(medias) as [name, url]}
+			<li>
+				<a
+					href={url}
+					target="_blank"
+					rel="noopener noreferrer"
+					class="text-blue-500 hover:underline"
+				>
+					{name}
+				</a>
+			</li>
+		{/each}
+	</ul>
 </section>
