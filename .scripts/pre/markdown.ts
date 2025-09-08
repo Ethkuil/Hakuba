acfd17378ead9963e6bf6909791e03ba46c56454
Ethkuil <ethkuil@gmail.com>
2025-09-09 02:34:55 +0800

    feat: add `remark-breaks` plugin

--- .scripts/pre/markdown.ts
+++ .scripts/pre/markdown.ts
@@ -11,6 +11,7 @@ import rehypeStringify from 'rehype-stringify'
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkParseFrontmatter from 'remark-parse-frontmatter';
 import remarkGfm from 'remark-gfm';
+import remarkBreaks from 'remark-breaks';
 
 // rehype plugins
 import rehypeSlug from 'rehype-slug'
@@ -23,6 +24,7 @@ const processor = unified()
   .use(remarkFrontmatter)
   .use(remarkParseFrontmatter)
   .use(remarkGfm)
+  .use(remarkBreaks)
   .use(remarkRehype, { allowDangerousHtml: true })
   .use(rehypeSlug)
   .use(rehypeAutolinkHeadings)
