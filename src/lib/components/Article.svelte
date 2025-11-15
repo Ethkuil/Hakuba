384d25e4c649986fda2d00069de991129aff3e37
Ethkuil <ethkuil@gmail.com>
2025-11-15 21:35:18 +0800

    feat: enable line numbering for `rehype-pretty-code`

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -2,8 +2,26 @@
 <!-- https://github.com/sindresorhus/github-markdown-css -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.8.1/github-markdown-light.min.css">
 <style>
+	/* customize github-markdown-css */
 	.markdown-body ul, ol {
 		all: revert;
+	}
+
+	/* customize rehype-pretty-code */
+	code {
+		counter-reset: line;
+	}
+
+	code > [data-line]::before {
+		counter-increment: line;
+		content: counter(line);
+
+		/* Other styling */
+		display: inline-block;
+		width: 0.75rem;
+		margin-right: 1rem;
+		text-align: right;
+		color: gray;
 	}
 </style>
 <!-- https://katex.org/docs/browser.html -->
