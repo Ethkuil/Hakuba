6fe15f4176838fc0c6a5368e31d0ba33ab48fe6e
Zhixin (Hobart) Lin <ethkuil@gmail.com>
2026-09-11 12:00:31 +0800

    Update AboutSection.svelte

--- src/lib/components/AboutSection.svelte
+++ src/lib/components/AboutSection.svelte
@@ -10,16 +10,15 @@
 			name: '知乎',
 			url: 'https://www.zhihu.com/people/epi3',
 			description:
-				'文字平台，内容最完整。各种文字类内容我都会发到知乎；其中值得长期保存的，会同步到个人网站。',
+				'文字平台。各种文字类内容我都会发到知乎；其中值得永久保存的，会同步到个人网站归档。',
 		},
-		{ name: '小红书', url: 'https://www.xiaohongshu.com/user/profile/660ae0f9000000001700d924' },
 		{ name: 'Bilibili', url: 'https://space.bilibili.com/472336190', description: '视频平台。' },
 		{
 			name: 'Bangumi',
 			url: 'https://bangumi.tv/user/ethkuil',
 			description: '用来记录和整理二次元作品的点格子工具。',
 		},
-		{ name: 'Last.fm', url: 'https://www.last.fm/user/Ethkuil', description: '记录最近听过的歌。' },
+		{ name: 'Last.fm', url: 'https://www.last.fm/user/Ethkuil', description: '记录最近听的歌。' },
 	];
 </script>
 
